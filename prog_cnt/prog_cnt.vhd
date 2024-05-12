be54f180a14857e9ff34120b3310b3350aab6957
Rafael Pereira <contato@rafael-pereira.com>
2024-05-11 21:12:27 -0300

    prog_cnt - Final
    Finalizado projeto pro_cnt Milleny.

--- prog_cnt/prog_cnt.vhd
+++ prog_cnt/prog_cnt.vhd
@@ -1,31 +1,41 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.numeric_std.ALL;
 
 entity prog_cnt is
-    Port ( clk : in STD_LOGIC;              -- Entrada de clock
+    Port ( clk_in : in STD_LOGIC;              -- Entrada de clock
            nrst : in STD_LOGIC;             -- Entrada de reset assíncrono
-           load : in STD_LOGIC;             -- Entrada de carga síncrona
-           data_in : in STD_LOGIC_VECTOR(10 downto 0);  -- Entrada de dados para carregamento
-           q : out STD_LOGIC_VECTOR(10 downto 0));      -- Saída do contador
+           new_pc_in : in STD_LOGIC_VECTOR(10 downto 0);  -- Entrada de dados para carregamento
+           from_stack : in STD_LOGIC_VECTOR(10 downto 0); -- Entrada para carga no contador, quando selecionado pela entrada pc_ctrl
+           pc_ctrl : in STD_LOGIC_VECTOR(1 downto 0); 
+           next_pc_out : out STD_LOGIC_VECTOR(10 downto 0); -- Saída do próximo valor do contador (combinacional)
+           pc_out : out STD_LOGIC_VECTOR(10 downto 0)); -- Saída do contador
 end prog_cnt;
 
 architecture arch1 of prog_cnt is
     signal cnt : unsigned(10 downto 0) := (others => '0');  -- Sinal interno para manter o estado do contador
+    signal next_cnt : unsigned(10 downto 0);
 begin
-    process(clk, nrst)
+    process(clk_in, nrst)
     begin
         if nrst = '0' then                 -- Se o sinal de reset assíncrono estiver em nível baixo
             cnt <= (others => '0');         -- Reseta o contador para zero
-        elsif rising_edge(clk) then         -- Caso contrário, na borda de subida do clock
-            if load = '1' then              -- Se o sinal de carga estiver ativo
-                cnt <= unsigned(data_in);   -- Carrega um novo valor para o contador a partir dos dados de entrada
-            else
-                cnt <= cnt + 1;             -- Incrementa o contador
-            end if;
+        elsif rising_edge(clk_in) then         -- Caso contrário, na borda de subida do clock
+            cnt <= next_cnt;             -- Atualiza o contador
         end if;
     end process;
-    
-	q <= std_logic_vector(to_unsigned(to_integer(cnt), 11));  -- Converte o valor do contador para um vetor de bits para a saída
+
+    process(cnt, new_pc_in, from_stack, pc_ctrl)
+    begin
+        case pc_ctrl is
+            when "00" => next_cnt <= cnt; -- Permanece como está
+            when "01" => next_cnt <= unsigned(new_pc_in); -- Carrega um novo valor para o contador
+            when "10" => next_cnt <= unsigned(from_stack); -- Carrega o valor presente no topo da pilha
+            when "11" => next_cnt <= cnt + 1; -- Incrementa o contador
+            when others => next_cnt <= cnt; -- Caso não definido, permanece como está
+        end case;
+    end process;
+
+    pc_out <= std_logic_vector(cnt);  -- Converte o valor do contador para um vetor de bits para a saída
+    next_pc_out <= std_logic_vector(next_cnt); -- Saída do próximo valor do contador
 end arch1;
